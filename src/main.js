fae98c54616d5a0764f735bc7a92094354bd09fd
mavek87 <matver87@gmail.com>
2025-03-06 22:21:12 +0100

    add action settings to UI

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ let actions = [];
 
 const selectAction = document.getElementById("select-action");
 const selectedActionDescription = document.getElementById('selected-action-description');
+const formActionProps = document.getElementById("form-action-props");
 
 async function notify_change_action(selectedActionValue, selectedActionName) {
     await invoke("notify_change_action", {value: selectedActionValue, name: selectedActionName});
@@ -25,7 +26,7 @@ window.addEventListener("DOMContentLoaded", async () => {
         actions = resultObj?.actions || [];
 
         for (const [action_key, action_props] of Object.entries(actions)) {
-            console.log(`key: ${action_key}, value: ${action_props}`);
+            console.log(`action key: ${action_key}, action props: ${action_props}`);
 
             const option = document.createElement('option');
             option.value = action_key;
@@ -36,6 +37,8 @@ window.addEventListener("DOMContentLoaded", async () => {
         selectAction.selectedIndex = 0;
         const action = actions[selectAction.value]
         selectedActionDescription.value = action.description;
+
+        populateSettingsForAction(action);
     }
 
     selectAction.addEventListener('change', function () {
@@ -43,6 +46,52 @@ window.addEventListener("DOMContentLoaded", async () => {
         const action = actions[actionValue]
         selectedActionDescription.value = action.description;
 
+        populateSettingsForAction(action);
+
         notify_change_action(actionValue, action.name)
     });
 });
+
+function populateSettingsForAction(action) {
+    formActionProps.innerHTML = '';
+
+    const maxElementsPerRow = 3;
+    let counterElementPerRow = 0;
+    let div;
+
+    for (const [action_default_key, action_default_value] of Object.entries(action.defaults)) {
+        console.log(`action key: ${action_default_key}`);
+        console.log(`action val: ${action_default_value}`);
+
+        if (counterElementPerRow % maxElementsPerRow === 0) {
+            div = document.createElement("div");
+            div.className = "grid";
+            formActionProps.appendChild(div);
+        }
+
+        const inputText = document.createElement('input');
+        inputText.type = 'text';
+        inputText.value = action_default_value;
+        inputText.id = 'form-action-props_input_' + action_default_key;
+
+        const labelText = document.createElement('label');
+        labelText.id = 'form-action-props_label_' + action_default_key
+        labelText.htmlFor = inputText.id
+        labelText.textContent = convertFirstCharToUppercase(convertSnakeToSpace(action_default_key));
+
+        const innerDiv = document.createElement("div");
+        innerDiv.appendChild(labelText);
+        innerDiv.appendChild(inputText);
+        div.appendChild(innerDiv);
+
+        counterElementPerRow++;
+    }
+}
+
+function convertSnakeToSpace(str) {
+    return str.replace(/_/g, ' ');
+}
+
+function convertFirstCharToUppercase(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
