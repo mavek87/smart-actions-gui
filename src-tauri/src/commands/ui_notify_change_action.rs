911364bc1b82b770d6236ccc81fd5eb5dcd7c9bc
mavek87 <matver87@gmail.com>
2025-03-08 21:47:14 +0100

    cleaning

--- src-tauri/src/commands/ui_notify_change_action.rs
+++ src-tauri/src/commands/ui_notify_change_action.rs
@@ -13,12 +13,6 @@ pub fn ui_notify_change_action(
     let smart_action: SmartAction =
         serde_json::from_str(&json_smart_action).expect("Failed to parse JSON Smart Action");
 
-    // state
-    //     .menu_manager
-    //     .lock()
-    //     .unwrap()
-    //     .set_action_name_text(format!("{}", smart_action.name));
-
     state
         .smart_action_manager
         .change_current_smart_action(smart_action);
