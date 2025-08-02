8b5e14535a3572d6a4dd20c4225c3cb91d1366a9
mavek87 <matver87@gmail.com>
2025-08-02 16:55:50 +0200

    updated dependencies and version

--- src-tauri/src/domain/constants.rs
+++ src-tauri/src/domain/constants.rs
@@ -1,5 +1,5 @@
 pub static APP_NAME: &str = "smart-actions-gui";
-pub static APP_VERSION: &str = "0.1.0";
+pub static APP_VERSION: &str = "1.0.0";
 pub static WEBSITE_LABEL: &str = "Github Repository";
 pub static WEBSITE: &str = "https://github.com/mavek87/smart-actions-gui";
 pub static AUTHORS: &[&str] = &["Matteo Veroni"];
